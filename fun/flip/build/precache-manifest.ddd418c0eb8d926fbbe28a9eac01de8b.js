263334d08633f36ac21ae5ea3993ec87d4695068
dichen.lzy <dichen.lzy@alibaba-inc.com>
2020-07-30 03:38:37 +0800

    chore: 完善功能

---
+++ fun/flip/build/precache-manifest.ddd418c0eb8d926fbbe28a9eac01de8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0675cb9a31637bb1ff323d4e930a02",
+    "url": "/mydemos/fun/flip/build/index.html"
+  },
+  {
+    "revision": "ab42c10cb4cd9ce00e66",
+    "url": "/mydemos/fun/flip/build/static/css/2.9e847b13.chunk.css"
+  },
+  {
+    "revision": "35229d9053ce548e45b6",
+    "url": "/mydemos/fun/flip/build/static/css/main.ba3c26f5.chunk.css"
+  },
+  {
+    "revision": "ab42c10cb4cd9ce00e66",
+    "url": "/mydemos/fun/flip/build/static/js/2.3d403c50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mydemos/fun/flip/build/static/js/2.3d403c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35229d9053ce548e45b6",
+    "url": "/mydemos/fun/flip/build/static/js/main.8c70bf00.chunk.js"
+  },
+  {
+    "revision": "61c1012ba4c69475bae5",
+    "url": "/mydemos/fun/flip/build/static/js/runtime-main.ec47a68b.js"
+  }
+]);
